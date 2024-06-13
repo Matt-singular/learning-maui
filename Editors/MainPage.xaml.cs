0bc6907a3f5956f9c4b7b8ccf4e345d239d72dac
Matthew Lewis <mlewis@singular.co.za>
2024-06-13 21:15:12 +0200

    Cleanup of editors

--- Editors/MainPage.xaml.cs
+++ Editors/MainPage.xaml.cs
@@ -1,23 +1,58 @@
 ﻿namespace Editors;
 
+using Microsoft.Maui.Storage;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 public partial class MainPage : ContentPage
 {
-  int count = 0;
-
   public MainPage()
   {
     InitializeComponent();
   }
 
-  private void OnCounterClicked(object sender, EventArgs e)
+  private void OnFormatJsonClicked(object sender, EventArgs e)
   {
-    count++;
+    try
+    {
+      var parsedJson = JToken.Parse(InputEditor.Text);
+      OutputEditor.Text = parsedJson.ToString(Formatting.Indented);
+    }
+    catch (JsonReaderException ex)
+    {
+      OutputEditor.Text = $"Invalid JSON: {ex.Message}";
+    }
+  }
 
-    if (count == 1)
-      CounterBtn.Text = $"Clicked {count} time";
-    else
-      CounterBtn.Text = $"Clicked {count} times";
+  private void OnClearClicked(object sender, EventArgs e)
+  {
+    InputEditor.Text = string.Empty;
+    OutputEditor.Text = string.Empty;
+  }
 
-    SemanticScreenReader.Announce(CounterBtn.Text);
+  private void OnCopyClicked(object sender, EventArgs e)
+  {
+    var text = OutputEditor.Text;
+    if (!string.IsNullOrEmpty(text))
+    {
+      Clipboard.SetTextAsync(text);
+    }
+  }
+
+  private async void OnSaveClicked(object sender, EventArgs e)
+  {
+    var filePickerResult = await FilePicker.Default.PickAsync(new PickOptions
+    {
+      FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.WinUI, new[] { "json" } }
+        }),
+      PickerTitle = "Save JSON File"
+    });
+
+    if (filePickerResult != null)
+    {
+      await File.WriteAllTextAsync(filePickerResult.FullPath, OutputEditor.Text);
+    }
   }
 }
