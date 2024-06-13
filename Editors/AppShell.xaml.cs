7cc92e122e69099ee4bd2bee57ac3c236b56369d
Matthew Lewis <mlewis@singular.co.za>
2024-06-13 21:12:43 +0200

    Testing with Maui Editor fields

---
+++ Editors/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Editors;
+
+public partial class AppShell : Shell
+{
+  public AppShell()
+  {
+    InitializeComponent();
+  }
+}
---
+++ Editors/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace Editors
+{
+  using Foundation;
+
+  [Register("AppDelegate")]
+  public class AppDelegate : MauiUIApplicationDelegate
+  {
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+  }
+}
