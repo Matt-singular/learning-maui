4c64938bd39a0149689f32e693b2ff35b2315923
Matthew Lewis <mlewis@singular.co.za>
2024-06-14 18:57:10 +0200

    Updated the AppShell to cater for changing SideMenu

--- SideMenus/AppShell.xaml.cs
+++ SideMenus/AppShell.xaml.cs
@@ -5,31 +5,54 @@ public partial class AppShell : Shell
   public string DefaultPage { get; set; }
   public bool HasSetUpProject { get; set; }
 
-  public AppShell()
+  public AppShell(bool hasSetup = false)
   {
+    this.HasSetUpProject = ChecksIfUserHasSetUpProject(hasSetup);
     InitializeComponent();
     InitialiseFlyoutItems();
-    GoToAsync($"//{DefaultPage}");
+  }
+
+  private bool ChecksIfUserHasSetUpProject(bool hasSetup)
+  {
+    if (hasSetup)
+    {
+      // User has set up the project
+      return true;
+    }
+
+    // Do a custom check to see if the user has set up the project
+    return HasSetUpProject;
   }
 
   /// <summary>
   /// Adds side menu entries programmatically
   /// </summary>
-  private void InitialiseFlyoutItems()
+  public void InitialiseFlyoutItems()
   {
+    this.Items.Clear();
     if (!HasSetUpProject)
     {
       // New user still needs to set up
-      AddFlyoutItem("Startup", "dotnet_bot.png", typeof(MainPage), true);
-      AddFlyoutItem("Configurations", "dotnet_bot.png", typeof(MainPage));
+      AddFlyoutItem("Startup", "dotnet_bot.png", typeof(StartupPage), true);
+
+      // Set flyout default behaviour
+      this.FlyoutBehavior = FlyoutBehavior.Disabled;
     }
     else
     {
       // Existing user
       AddFlyoutItem("Home", "dotnet_bot.png", typeof(MainPage), true);
+      AddFlyoutItem("Help", "dotnet_bot.png", typeof(MainPage));
       AddFlyoutItem("Configurations", "dotnet_bot.png", typeof(MainPage));
       AddFlyoutItem("User screen 1", "dotnet_bot.png", typeof(MainPage));
+
+      // Set flyout default behaviour
+      this.FlyoutBehavior = FlyoutBehavior.Locked;
     }
+
+    // Navigate to the default page
+    GoToAsync($"//{DefaultPage}");
+
   }
 
   private void AddFlyoutItem(string title, string icon, Type navigationPageType, bool isDefaultPage = false)
