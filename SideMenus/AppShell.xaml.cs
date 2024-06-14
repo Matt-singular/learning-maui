2b99f2f892fff4886c2cea757c7cdffb3b5e6a5d
Matthew Lewis <mlewis@singular.co.za>
2024-06-14 16:58:38 +0200

    Added basic side menu

--- SideMenus/AppShell.xaml.cs
+++ SideMenus/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace SideMenus
+﻿namespace SideMenus;
+
+public partial class AppShell : Shell
 {
-  public partial class AppShell : Shell
+  public AppShell()
   {
-    public AppShell()
-    {
-      InitializeComponent();
-    }
+    InitializeComponent();
   }
 }
---
+++ SideMenus/MenuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SideMenus;
+
+using System.Windows.Input;
+
+public partial class MenuPage : ContentPage
+{
+  public ICommand NavigateCommand { get; private set; }
+
+  public MenuPage()
+  {
+    InitializeComponent();
+
+    NavigateCommand = new Command<string>(async (route) => await Shell.Current.GoToAsync(route));
+  }
+}
