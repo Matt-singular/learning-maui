e67cb548c2ea9b3d444a97eb74fad887edd1e525
Matthew Lewis <mlewis@singular.co.za>
2024-06-14 16:49:26 +0200

    Scaffoled the SideMenus project

---
+++ SideMenus/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SideMenus
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
---
+++ SideMenus/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SideMenus
+{
+  public partial class AppShell : Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+    }
+  }
+}
