26a6fc4661346c3d03dce0ecefc112eb014e8f81
Matthew Lewis <mlewis@singular.co.za>
2024-06-14 18:57:29 +0200

    Added the StartupPage

---
+++ SideMenus/StartupPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace SideMenus;
+
+public partial class StartupPage : ContentPage
+{
+  int count = 0;
+
+  public StartupPage()
+  {
+    InitializeComponent();
+  }
+
+  private void OnStartupWizardClicked(object sender, EventArgs e)
+  {
+    if (Application.Current.MainPage is AppShell appShell)
+    {
+      //appShell.HasSetUpProject = true;
+      //appShell.InitialiseFlyoutItems();
+      Application.Current.MainPage = new AppShell(true);
+    }
+  }
+}
