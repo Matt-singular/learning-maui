c8dc4b4c5730ddabf421f1f1b731ff65d22c4ccc
Matthew Lewis <mlewis@singular.co.za>
2024-06-14 16:20:48 +0200

    Added project to experiment with dropdown fields

---
+++ Dropdowns/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Dropdowns;
+
+using Microsoft.Maui.Controls;
+
+public partial class MainPage : ContentPage
+{
+  public double QuarterScreenWidth { get; set; }
+
+  public MainPage()
+  {
+    InitializeComponent();
+    QuarterScreenWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density / 4;
+    this.BindingContext = this;
+    PopulateComboBox();
+  }
+
+  private void PopulateComboBox()
+  {
+    var options = new List<string>
+    {
+      "Option 1",
+      "Option 2",
+      "Option 3",
+      "Option 4"
+    };
+
+    HardcodedDropdown.ItemsSource = options;
+  }
+
+  private void OnChangeSelectedDropdownOption(object sender, EventArgs e)
+  {
+    // If there is custom logic to fire when changing the dropdown selection:
+    var selectedOption = HardcodedDropdown.SelectedItem.ToString();
+    Console.WriteLine($"Selected: {selectedOption}");
+  }
+}
---
+++ Dropdowns/README.md
@@ -0,0 +1,6 @@
+# Dropdowns
+This project focuses on learning the ins-and-outs of the dropdown `Picker` field.
+
+## Focus files
+- **MainPage.xaml** -  contains the `Picker` fields
+- **Mainpage.xaml.cs** - contains the code behind for the `Picker` fields
