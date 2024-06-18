4b95350381179705e75bff0d25b7277e362bba72
Matthew Lewis <mlewis@singular.co.za>
2024-06-18 21:45:51 +0200

    Added the SearchBar

--- EntryFields/MainPage.xaml.cs
+++ EntryFields/MainPage.xaml.cs
@@ -4,6 +4,7 @@ public partial class MainPage : ContentPage
   public Entry BuildInputField { get; private set; }
   public Entry FromInputField { get; private set; }
   public Entry ToInputField { get; private set; }
+  public SearchBar SearchField { get; private set; }
 
   public MainPage()
   {
@@ -13,6 +14,7 @@ public partial class MainPage : ContentPage
     this.BuildInputField = RenderRow(columnWidth, Constants.BuildLabel, Constants.BuildPlaceholder, OnClickBuildInputField);
     this.FromInputField = RenderRow(columnWidth, Constants.FromLabel, Constants.FromPlaceholder, OnClickFromInputField);
     this.ToInputField = RenderRow(columnWidth, Constants.ToLabel, Constants.ToPlaceholder, OnClickToInputField);
+    this.SearchField = RenderSearchRow(OnSearchField);
   }
 
   #region Event Handlers
@@ -20,6 +22,7 @@ public partial class MainPage : ContentPage
   private void OnClickBuildInputField(object? sender, EventArgs e) => DisplayAlert("BuildInputField Value", this.BuildInputField.Text, "OK");
   private void OnClickFromInputField(object? sender, EventArgs e) => DisplayAlert("FromInputField Value", this.FromInputField.Text, "OK");
   private void OnClickToInputField(object? sender, EventArgs e) => DisplayAlert("ToInputField Value", this.ToInputField.Text, "OK");
+  private void OnSearchField(object? sender, EventArgs e) => DisplayAlert("SearchField Value", this.SearchField.Text, "OK");
   #endregion
 
   #region XAML
@@ -92,6 +95,39 @@ public partial class MainPage : ContentPage
 
     // Return the entry to allow binding in the constructor
     return entry;
+  }
+
+  private SearchBar RenderSearchRow(EventHandler onSearchEvent)
+  {
+    // Create the Grid
+    var grid = new Grid
+    {
+      HorizontalOptions = LayoutOptions.FillAndExpand,
+      VerticalOptions = LayoutOptions.Start,
+      ColumnSpacing = 50
+    };
+
+    // Define the columns
+    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+
+    // Create the SearchBar
+    var searchBar = new SearchBar
+    {
+      Placeholder = "Search...",
+      VerticalOptions = LayoutOptions.Center,
+      HorizontalOptions = LayoutOptions.FillAndExpand
+    };
+    searchBar.SearchButtonPressed += onSearchEvent;
+    Grid.SetColumn(searchBar, 0);
+    Grid.SetColumnSpan(searchBar, 2); // Make the SearchBar span across both columns
+
+    // Add the SearchBar to the Grid
+    grid.Children.Add(searchBar);
+
+    // Add the Grid to the ContentPage
+    AddToContentPage(grid);
+    return searchBar;
   }
 
   private void AddToContentPage(Layout layoutToAdd)
--- EntryFields/README.md
+++ EntryFields/README.md
@@ -1,6 +1,6 @@
 # EntryFields
-This project focuses on learning the ins-and-outs of the input `Entry` field.
+This project focuses on learning the ins-and-outs of the input `Entry` and `SearchBar` fields.
 
 ## Focus files
-- **MainPage.xaml** -  contains the `Entry` fields
-- **Mainpage.xaml.cs** - contains the code behind for the `Entry` fields as well as alternative syntax for programmatically creating the `Entry` fields.
+- **MainPage.xaml** -  contains the `Entry` fields in xaml form
+- **Mainpage.xaml.cs** - contains the code behind for the `Entry` fields as well as alternative syntax for programmatically creating the `Entry` and `SearchBar` fields.
